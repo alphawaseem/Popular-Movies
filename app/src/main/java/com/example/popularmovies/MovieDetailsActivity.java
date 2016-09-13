ebdec8cae54d6e0acb930b149548472ba971eeaf
Waseem Ahmed <alphawaseem@gmail.com>
2016-09-13 21:47:20 +0530

    update view with data

--- app/src/main/java/com/example/popularmovies/MovieDetailsActivity.java
+++ app/src/main/java/com/example/popularmovies/MovieDetailsActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 
@@ -18,7 +19,23 @@ public class MovieDetailsActivity extends AppCompatActivity {
         setContentView(R.layout.movie_detail);
         Intent intent = getIntent();
         Movies movie = intent.getParcelableExtra(MainActivity.MOVIE);
+
+
         ImageView imageView = (ImageView) findViewById(R.id.detail_movie_poster);
+        TextView titleView = (TextView) findViewById(R.id.title_field);
+        TextView originalView = (TextView) findViewById(R.id.original_title_field);
+        TextView releaseDateView = (TextView) findViewById(R.id.date_field);
+        TextView voteView = (TextView) findViewById(R.id.votes_field);
+        TextView overviewField = (TextView) findViewById(R.id.overview_field);
+
         Picasso.with(this).load(movie.getPosterUrl()).fit().into(imageView);
+
+        titleView.setText(movie.getTitle());
+        originalView.setText(movie.getOriginalTitle());
+        releaseDateView.setText(movie.getReleaseDate());
+        voteView.setText(movie.getVoteCount());
+        overviewField.setText(movie.getOverview());
+
+
     }
 }
