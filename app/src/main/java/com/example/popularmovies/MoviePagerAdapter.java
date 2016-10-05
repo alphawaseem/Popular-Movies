99b6f28093bc468852beb02e8bdccc98eb11294c
Waseem Ahmed <alphawaseem@gmail.com>
2016-10-05 20:02:43 +0530

    working viewpager

---
+++ app/src/main/java/com/example/popularmovies/MoviePagerAdapter.java
@@ -0,0 +1,46 @@
+package com.example.popularmovies;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+/**
+ * Created by peace on 5/10/16.
+ */
+
+public class MoviePagerAdapter extends FragmentStatePagerAdapter {
+
+    Context context;
+
+    public MoviePagerAdapter(Context context, FragmentManager fm) {
+        super(fm);
+        this.context = context;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        if (position == 0)
+            return PopularFragment.newInstance();
+        return PopularFragment.newInstance();
+    }
+
+    @Override
+    public int getCount() {
+        return 2;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        String title = null;
+        switch (position) {
+            case 0:
+                title = "Popular";
+                break;
+            case 1:
+                title = "Top Rated";
+                break;
+        }
+        return title;
+    }
+}
---
+++ app/src/main/java/com/example/popularmovies/PopularFragment.java
@@ -0,0 +1,73 @@
+package com.example.popularmovies;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class PopularFragment extends Fragment {
+
+    final static String TAG = PopularFragment.class.getSimpleName();
+    // TODO - insert your themoviedb.org API KEY here
+    private final static String API_KEY = ApiKey.getApiKey();
+    RecyclerView.LayoutManager mLayoutManager;
+    MoviesAdapter adapter;
+    RecyclerView recyclerView;
+    private List<Movie> movieList;
+
+    static PopularFragment newInstance() {
+        PopularFragment fragment = new PopularFragment();
+        return fragment;
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+
+        View rootView = inflater.inflate(R.layout.recycle_view, container, false);
+        recyclerView = (RecyclerView) rootView.findViewById(R.id.recycler_view);
+        mLayoutManager = new GridLayoutManager(getActivity(), 2);
+        recyclerView.setLayoutManager(mLayoutManager);
+
+        RetrofitApiInterface apiService =
+                RetrofitApiClient.getClient().create(RetrofitApiInterface.class);
+
+        Call<MoviesResponse> call = apiService.getPopularMovies(API_KEY);
+        call.enqueue(new Callback<MoviesResponse>() {
+            @Override
+            public void onResponse(Call<MoviesResponse> call, Response<MoviesResponse> response) {
+                movieList = response.body().getResults();
+                adapter = new MoviesAdapter(movieList, getContext());
+                //recyclerView.addItemDecoration(new GridSpacingItemDecoration(2, dpToPx(10), true));
+                recyclerView.setItemAnimator(new DefaultItemAnimator());
+                recyclerView.setAdapter(adapter);
+            }
+
+            @Override
+            public void onFailure(Call<MoviesResponse> call, Throwable t) {
+                // Log error here since request failed
+                Log.e(TAG, t.toString());
+            }
+        });
+        return rootView;
+    }
+
+
+}
