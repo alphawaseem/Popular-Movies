5873efa51c9be0b59d0f9ed18e8e364dc1d6f8b4
Waseem Ahmed <alphawaseem@gmail.com>
2016-10-07 13:02:50 +0530

    make detail layout

--- app/src/main/java/com/example/popularmovies/DetailFragment.java
+++ app/src/main/java/com/example/popularmovies/DetailFragment.java
@@ -7,19 +7,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 
-import butterknife.BindView;
 import butterknife.ButterKnife;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class DetailFragment extends Fragment {
-
-    @BindView(R.id.detail_poster)
-    ImageView poster;
 
     public static DetailFragment newInstance() {
         return new DetailFragment();
@@ -30,9 +27,11 @@ public class DetailFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.detail_movie_card, container, false);
         ImageView photo = ButterKnife.findById(view, R.id.detail_poster);
+        TextView title = ButterKnife.findById(view, R.id.detail_title);
 
         Movie movie = getActivity().getIntent().getParcelableExtra("MOVIE");
         Picasso.with(getContext()).load("http://image.tmdb.org/t/p/w185/" + movie.getPosterPath()).fit().into(photo);
+        title.setText(movie.getTitle());
         return view;
     }
 }
