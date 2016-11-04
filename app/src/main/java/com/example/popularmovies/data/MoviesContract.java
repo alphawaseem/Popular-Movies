e6ac7458216f5d0f73e83d3123ce99a015925753
Waseem Ahmed <alphawaseem@gmail.com>
2016-11-04 14:43:06 +0530

    refactor

--- app/src/main/java/com/example/popularmovies/data/MoviesContract.java
+++ app/src/main/java/com/example/popularmovies/data/MoviesContract.java
@@ -16,10 +16,6 @@ public class MoviesContract {
     public static final String AUTHORITY = "com.example.popularmovies.data.MoviesProvider";
     private static final int MOVIES = 1;
     private static final int MOVIE_ITEM = 2;
-    private static final int REVIEWS = 3;
-    private static final int REVIEW_ITEM = 4;
-    private static final int VIDEOS = 5;
-    private static final int VIDEO_ITEM = 6;
     private static final UriMatcher MATCHER;
 
     static {
@@ -28,11 +24,6 @@ public class MoviesContract {
                 "movies", MOVIES);
         MATCHER.addURI(AUTHORITY,
                 "movies/#", MOVIE_ITEM);
-        MATCHER.addURI(AUTHORITY, "reviews", REVIEWS);
-        MATCHER.addURI(AUTHORITY, "reviews/#", REVIEW_ITEM);
-        MATCHER.addURI(AUTHORITY, "videos", VIDEOS);
-        MATCHER.addURI(AUTHORITY, "videos/#", VIDEO_ITEM);
-
     }
 
     private MoviesContract() {
@@ -47,18 +38,6 @@ public class MoviesContract {
                 break;
             case MOVIE_ITEM:
                 type = "vnd.popularmovies.cursor.item/movie";
-                break;
-            case REVIEWS:
-                type = "vnd.popularmovies.cursor.dir/review";
-                break;
-            case REVIEW_ITEM:
-                type = "vnd.popularmovies.cursor.item/review";
-                break;
-            case VIDEOS:
-                type = "vnd.popularmovies.cursor.dir/video";
-                break;
-            case VIDEO_ITEM:
-                type = "vnd.popularmovies.cursor.item/video";
                 break;
         }
         return type;
@@ -81,30 +60,4 @@ public class MoviesContract {
         public static final int INDEX_VOTE_AVG = 5;
         public static final int INDEX_POSTER_PATH = 6;
     }
-
-    public static class ReviewsEntry implements BaseColumns {
-        public static final String REVIEWS_TABLE = "reviews";
-        public static final String COL_AUTHOR = "author";
-        public static final String COL_REVIEW = "review";
-        public static final String COL_MOVIE_ID = "movie_id";
-
-        public static final int INDEX_ID = 0;
-        public static final int INDEX_AUTHOR = 1;
-        public static final int INDEX_REVIEW = 2;
-        public static final int INDEX_MOVIE_ID = 3;
-    }
-
-    public static class TrailerEntry implements BaseColumns {
-        public static final String TRAILER_TABLE = "trailers";
-        public static final String COL_KEY = "key";
-        public static final String COL_NAME = "name";
-        public static final String COL_MOVIE_ID = "movie_id";
-
-        public static final int INDEX_ID = 0;
-        public static final int INDEX_KEY = 1;
-        public static final int INDEX_NAME = 2;
-        public static final int INDEX_MOVIE_ID = 3;
-    }
-
-
 }
--- app/src/main/java/com/example/popularmovies/data/MoviesDbHelper.java
+++ app/src/main/java/com/example/popularmovies/data/MoviesDbHelper.java
@@ -23,30 +23,11 @@ public class MoviesDbHelper extends SQLiteOpenHelper {
             MoviesContract.MoviesEntry.COL_MOVIE_POSTER_PATH + " TEXT NOT NULL" +
             ");";
 
-    private static String SQL_CREATE_REVIEW_TABLE = "CREATE TABLE " + MoviesContract.ReviewsEntry.REVIEWS_TABLE +
-            "( " + MoviesContract.ReviewsEntry._ID + " INTEGER PRIMARY KEY ," +
-            MoviesContract.ReviewsEntry.COL_AUTHOR + " TEXT ," +
-            MoviesContract.ReviewsEntry.COL_REVIEW + " TEXT NOT NULL, " +
-            MoviesContract.ReviewsEntry.COL_MOVIE_ID + " INTEGER NOT NULL , " +
-            "FOREIGN KEY(" + MoviesContract.ReviewsEntry.COL_MOVIE_ID + ")" +
-            "REFERENCES " + MoviesContract.MoviesEntry.MOVIES_TABLE + "(" +
-            MoviesContract.MoviesEntry._ID + ")" + ");";
-
-    private static String SQL_CREATE_TRAILER_TABLE = "CREATE TABLE " + MoviesContract.TrailerEntry.TRAILER_TABLE +
-            "( " + MoviesContract.TrailerEntry._ID + " INTEGER PRIMARY KEY ," +
-            MoviesContract.TrailerEntry.COL_KEY + " TEXT ," +
-            MoviesContract.TrailerEntry.COL_NAME + " TEXT NOT NULL, " +
-            MoviesContract.TrailerEntry.COL_MOVIE_ID + " INTEGER NOT NULL , " +
-            "FOREIGN KEY(" + MoviesContract.TrailerEntry.COL_MOVIE_ID + ")" +
-            "REFERENCES " + MoviesContract.MoviesEntry.MOVIES_TABLE + "(" +
-            MoviesContract.MoviesEntry._ID + ")" + ");";
 
 
 
 
     private static String SQL_DELETE_MOVIES_TABLE = "DROP TABLE IF EXISTS " + MoviesContract.MoviesEntry.MOVIES_TABLE;
-    private static String SQL_DELETE_REVIEWS_TABLE = "DROP TABLE IF EXISTS " + MoviesContract.ReviewsEntry.REVIEWS_TABLE;
-    private static String SQL_DELETE_TRAILER_TABLE = "DROP TABLE IF EXISTS " + MoviesContract.TrailerEntry.TRAILER_TABLE;
 
 
     public MoviesDbHelper(Context context) {
@@ -56,15 +37,11 @@ public class MoviesDbHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(SQL_CREATE_MOVIE_TABLE);
-        db.execSQL(SQL_CREATE_REVIEW_TABLE);
-        db.execSQL(SQL_CREATE_TRAILER_TABLE);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL(SQL_DELETE_MOVIES_TABLE);
-        db.execSQL(SQL_DELETE_REVIEWS_TABLE);
-        db.execSQL(SQL_DELETE_TRAILER_TABLE);
         onCreate(db);
     }
 
